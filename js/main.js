4ac48b454e240b21c152f60f57c453f7c585b61f
Wilmer Florez <florez.wilmer27@gmail.com>
2024-05-25 22:30:03 -0500

    agregar sprint3

--- js/main.js
+++ js/main.js
@@ -1,125 +1,276 @@
 const data = {
     currentDate: "2023-01-01",
     events: [
-        {
-            name: "2 Wheels Fair",
-            image: "https://i.postimg.cc/0y5CJ7Hx/800-X-600-300x300.png",
-            date: "2022-12-12",
-            description: "the event with the greatest commercial impact of the motorcycle industry in America.",
-            price: 15,
-        },
-        {
-            name: "Tribute to the Mother of Mothers",
-            image: "https://i.postimg.cc/13YDn2qc/Whats-App-Image-2024-04-19-at-11-44-45-AM-300x300.jpg",
-            date: "2023-08-12",
-            description: "The great concert of the international choir and symphony orchestra of the Knights of the Virgin, with Gregorian music.",
-            price: 15,
-        },
-        {
-            name: "Vape South America Medellin 2024",
-            image: "https://i.postimg.cc/T3PrPfp3/Vape-South-America-Listing-800x600-1-300x300.jpg",
-            date: "2022-11-02",
-            description: "This edition promises to be the largest meeting point for vape enthusiasts, with a renewed look and focus on promoting the vaping industry.",
-            price: 15,
-        },
-        {
-            name: "PMU Industry Expo",
-            image: "https://i.postimg.cc/FzgjYKy7/Expoindustry-300x300.jpg",
-            date: "2023-11-02",
-            description: "It is the ideal space to promote brands and make commercial alliances.",
-            price: 15,
-        },
-        {
-            name: "Expotatuaje Medellín 2024",
-            image: "https://i.postimg.cc/59w57shb/Expotatuaje1-300x300.jpg",
-            date: "2022-02-12",
-            description: "This year the Expo will have significant growth and will be the incorporation of a new room, which will be the white room.",
-            price: 15,
-        },
-        {
-            name: "Expopartes Autopartes Medellin Fair",
-            image: "https://i.postimg.cc/kXqQkxC8/LOGO-AUTOPARTES-Mesa-de-trabajo-1-300x300.png",
-            date: "2023-02-12",
-            description: "Asopartes has developed one of the most representative after-sales fairs of the automotive industry in Latin America.",
-            price: 15,
-        },
-        {            
-            name: "Expobelleza 2024",
-            image: "https://i.postimg.cc/brjxYn27/Expobelleza-2024-800x600-1-300x300.jpg",
-            date: "2023-01-22",
-            description: "Expobelleza Medellín is the business platform that connects multiple national and international brands interested in opening new markets in Medellín, Colombia and the world.",
-            price: 15,
-        },
-        {            
-            name: "Business and Entrepreneurship Fair",
-            image: "https://i.postimg.cc/cLDB76V3/BANNER-EN-PAGINA-DE-PLAZA-Mesa-de-trabajo-1-3-300x300.png",
-            date: "2022-01-22",
-            description: "It seeks to bring together businessmen and entrepreneurs from different sectors of the economy, being the opportunity for them to make their business visible and promote their products and services.",
-            price: 15,
-        },
-        {            
-            name: "XII National Convention of Alcoholics Anonymous",
-            image: "https://i.postimg.cc/g0X8vsy9/XII-Convencion-Nacional-de-AA-300x300.jpg",
-            date: "2022-03-01",
-            description: "event held every five years in which topics are developed in community with exhibitors and the attending public.",
-            price: 15,
-        },
-        {            
-            name: "Expoartesano La Memoria 2024",
-            image: "https://i.postimg.cc/k4SNdsDN/expoartesano-la-memoria-2024-300x300.png",
-            date: "2023-03-01",
-            description: "It is an event that exalts the memory, traditions and ancestral customs of Colombia.",
-            price: 15,            
-        },
-        {            
-            name: "Colombiamoda 2024",
-            image: "https://i.postimg.cc/0NbdVPsB/banners-colmodatex-24-plaza-mayor-800x600-300x300.jpg",
-            date: "2023-10-15",
-            description: "SPACES OF INSPIRATION and FASHION EXPRESSIONS in finished products, and together with Colombiatex activates business in the second half of the year for the textile chain in the region.",
-            price: 15,
-        },
-        {           
-            name: "Expo Mobile",
-            image: "https://i.postimg.cc/bNgx8sY5/sitio-web-300x300.jpg",
-            date: "2022-11-09",
-            description: "the most complete commercial exhibition of the LATAM cell phone industry.",
-            price: 15,
-        },
-        {           
-            name: "EFFIX Electronic Commerce Fair",
-            image: "https://i.postimg.cc/MHyY15SZ/800-x-600-300x300.jpg",
-            date: "2022-3-11",
-            description: "It brings together various E-commerce actors, including service and product providers.",
-            price: 15,
-        },
-        {            
-            name: "Expocamacol 2024",
-            image: "https://i.postimg.cc/rFkjMB1D/Banner-800-x-600-Expo-CAMACOL2024-1-300x300.jpg",
-            date: "2023-10-15",
-            description: "the International Construction, Engineering, Architecture and Design Fair.",
-            price: 15,
-        },
+      {
+        _id: "639c723b992482e5f2834be9",
+        name: "Collectivities Party",
+        image: "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
+        date: "2022-12-12",
+        description:
+          "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+        category: "Food Fair",
+        place: "Room A",
+        capacity: 45000,
+        assistance: 42756,
+        price: 5,
+        __v: 0,
+      },
+      {
+        _id: "639c723b992482e5f2834beb",
+        name: "Korean style",
+        image: "https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
+        date: "2023-08-12",
+        description:
+          "Enjoy the best Korean dishes, with international chefs and awesome events.",
+        category: "Food Fair",
+        place: "Room A",
+        capacity: 45000,
+        price: 10,
+        __v: 0,
+        estimate: 42756,
+      },
+      {
+        _id: "639c723c992482e5f2834bed",
+        name: "Jurassic Park",
+        image: "https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
+        date: "2022-11-02",
+        description:
+          "Let's go meet the biggest dinosaurs in the paleontology museum.",
+        category: "Museum",
+        place: "Field",
+        capacity: 82000,
+        price: 15,
+        __v: 0,
+        assistance: 65892,
+      },
+      {
+        _id: "639c723c992482e5f2834bef",
+        name: "Parisian Museum",
+        image: "https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
+        date: "2023-11-02",
+        description:
+          "A unique tour in the city of lights, get to know one of the most iconic places.",
+        category: "Museum",
+        place: "Paris",
+        capacity: 8200,
+        estimate: 8200,
+        price: 3500,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf1",
+        name: "Comicon",
+        image: "https://i.postimg.cc/KYD0jMf2/comicon.jpg",
+        date: "2022-02-12",
+        description:
+          "For comic lovers, all your favourite characters gathered in one place.",
+        category: "Costume Party",
+        place: "Room C",
+        capacity: 120000,
+        assistance: 110000,
+        price: 54,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf3",
+        name: "Halloween Night",
+        image: "https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
+        date: "2023-02-12",
+        description: "Come with your scariest costume and win incredible prizes.",
+        category: "Costume Party",
+        place: "Room C",
+        capacity: 12000,
+        estimate: 9000,
+        price: 12,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf5",
+        name: "Metallica in concert",
+        image: "https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
+        date: "2023-01-22",
+        description: "The only concert of the most emblematic band in the world.",
+        category: "Music Concert",
+        place: "Room A",
+        capacity: 138000,
+        estimate: 138000,
+        price: 150,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf7",
+        name: "Electronic Fest",
+        image: "https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
+        date: "2022-01-22",
+        description:
+          "The best national and international DJs gathered in one place.",
+        category: "Music Concert",
+        place: "Room A",
+        capacity: 138000,
+        assistance: 110300,
+        price: 250,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834bf9",
+        name: "10K for life",
+        image: "https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
+        date: "2022-03-01",
+        description: "Come and exercise, improve your health and lifestyle.",
+        category: "Race",
+        place: "Soccer field",
+        capacity: 30000,
+        assistance: 25698,
+        price: 3,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834bfb",
+        name: "15K NY",
+        image: "https://i.postimg.cc/zv67r65z/15kny.jpg",
+        date: "2023-03-01",
+        description:
+          "We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
+        category: "Race",
+        place: "New York",
+        capacity: 3000000,
+        price: 3,
+        __v: 0,
+        estimate: 2569800,
+      },
+      {
+        _id: "639c723d992482e5f2834bfd",
+        name: "School's book fair",
+        image: "https://i.postimg.cc/Sst763n6/book1.jpg",
+        date: "2023-10-15",
+        description: "Bring your unused school book and take the one you need.",
+        category: "Book Exchange",
+        place: "Room D1",
+        capacity: 150000,
+        estimate: 123286,
+        price: 1,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834bff",
+        name: "Just for your kitchen",
+        image: "https://i.postimg.cc/05FhxHVK/book4.jpg",
+        date: "2022-11-09",
+        description:
+          "If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
+        category: "Book Exchange",
+        place: "Room D6",
+        capacity: 130000,
+        assistance: 90000,
+        price: 100,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834c01",
+        name: "Batman",
+        image: "https://i.postimg.cc/vH52y81C/cinema4.jpg",
+        date: "2022-3-11",
+        description: "Come see Batman fight crime in Gotham City.",
+        category: "Cinema",
+        place: "Room D1",
+        capacity: 11000,
+        assistance: 9300,
+        price: 225,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834c03",
+        name: "Avengers",
+        image: "https://i.postimg.cc/T3C92KTN/scale.jpg",
+        date: "2023-10-15",
+        description:
+          "Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
+        category: "Cinema",
+        place: "Room D1",
+        capacity: 9000,
+        estimate: 9000,
+        price: 250,
+        __v: 0,
+      },
     ],
-};
+  };
 
-let cardsHome = document.getElementById("cardsHome");
-for (let i = 0; i < data.events.length; i++) {
-    let card = document.createElement("div");
-    card.innerHTML = `
-    <div class="card me-3 my-1 h-100 cards-i" style="width: 18rem">
-        <img src="${data.events[i].image}" class="card-img-top" alt="${data.events[i].name}" style="height: 20vh; object-fit: cover;"/>
-        <div class="card-body text-center d-flex flex-column justify-content-around">
-            <h5 class="card-title">${data.events[i].name}</h5>
-            <p class="card-text">
-                ${data.events[i].description}
-            </p>
-            <div class="d-flex justify-content-around align-items-baseline">
-                <p>US ${data.events[i].price} USD</p>
-                <a href="./details.html" class="btn go-details">Details</a>
+  
+function crearCard(events) {
+    let cardsHome = document.getElementById("cardsHome");
+    cardsHome.innerHTML = '';
+    for (let i = 0; i < events.length; i++) {
+        let card = document.createElement("div");
+        card.innerHTML = `
+        <div class="card me-3 my-1 h-100 cards-i" style="width: 18rem">
+            <img src="${events[i].image}" class="card-img-top" alt="${events[i].name}" style="height: 20vh; object-fit: cover;"/>
+            <div class="card-body text-center d-flex flex-column justify-content-around">
+                <h5 class="card-title">${events[i].name}</h5>
+                <p class="card-text">
+                    ${events[i].description}
+                </p>
+                <div class="d-flex justify-content-around align-items-baseline">
+                    <p>US ${events[i].price} USD</p>
+                    <a href="./details.html" class="btn go-details">Details</a>
+                </div>
             </div>
         </div>
-    </div>
-    `;
-    card.classList.add("my-3");
-    cardsHome.appendChild(card);
+        `;
+        card.classList.add("my-3");
+        cardsHome.appendChild(card);
+    }
 }
+
+crearCard(data.events);
+
+
+function unificarCategorias(events) {
+  
+  let categorias = new Set();
+
+  
+  events.forEach(event => {
+    categorias.add(event.category);
+  });
+
+ 
+  let vectorCategories = Array.from(categorias);
+
+  
+  return vectorCategories;
+}
+
+
+let vectorCategories = unificarCategorias(data.events);
+
+
+
+
+for(let i=0; i<vectorCategories.length; i++){
+    let check = document.createElement("div");
+    check.innerHTML=`
+      
+          <input class="form-check-input" type="checkbox" id="category-${i}" value="${vectorCategories[i]}">
+          <label class="form-check-label" for="category-${i}">${vectorCategories[i]}</label>
+      `;
+     check.classList.add("form-check","form-check-inline");
+      
+      document.getElementById("Checks").appendChild(check);
+}
+
+
+
+function filterCards() {
+  let selectedCategories = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(cb => cb.value);
+  if (selectedCategories.length === 0) {
+    crearCard(data.events);
+  } else {
+      let filteredEvents = data.events.filter(event => selectedCategories.includes(event.category));
+      crearCard(filteredEvents);
+  }
+}
+
+
+document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+  checkbox.addEventListener('change', filterCards);
+});
